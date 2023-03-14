30c92c2b015f57b0265f70f85d029df80def8466
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-14 09:21:48 +0000

    Progress.

--- src/assets/components/navigation/Navbar.jsx
+++ src/assets/components/navigation/Navbar.jsx
@@ -5,12 +5,15 @@ import {auth} from '../../config/FirebaseConfig'
 import { useNavigate } from 'react-router'
 import { AppContext } from '../../context/AppContext'
 
-
+//Icons
+import {RxDashboard} from 'react-icons/rx'
 
 
 function Navbar() {
 const user = JSON.parse(localStorage.getItem("user"))
+
 const navigate = useNavigate()
+
 async function logOut(){
   await signOut(auth)
   localStorage.removeItem("user")
@@ -23,6 +26,30 @@ async function logOut(){
     <nav className='bg-[#654330] w-[15%] text-white fixed p-5 h-screen'>
       <p>{user.email}</p>
       <hr className='my-5'/>
+      {/* Buttons */}
+      <section className='flex flex-col justify-around h-[50%]'>
+          <div className='flex items-center gap-5'>
+              <RxDashboard/>
+              <p>Dashboard</p>
+          </div>
+          <div className='flex items-center gap-5'>
+              <RxDashboard/>
+              <p>Rooms</p>
+          </div>
+          <div className='flex items-center gap-5'>
+              <RxDashboard/>
+              <p>Tenants</p>
+          </div>
+          <div className='flex items-center gap-5'>
+              <RxDashboard/>
+              <p>Tenants</p>
+          </div>
+          <div className='flex items-center gap-5'>
+              <RxDashboard/>
+              <p>Tenants</p>
+          </div>
+      </section>
+      <br />
       <button className='border px-4 py-2' onClick={logOut}>Sign Out</button>
     </nav>
   )
